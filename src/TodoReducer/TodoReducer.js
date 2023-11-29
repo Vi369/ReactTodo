de88ddeada930e820352cebdf332f1451f6dcd3e
vi369 <vikashverma3001@gmai.com>
2023-11-29 14:42:12 +0530

    understanding the concept of reducer

---
+++ src/TodoReducer/TodoReducer.js
@@ -0,0 +1,17 @@
+
+function TodoReducer(state, action) {
+    if(action.type =='add_todo'){
+
+    }else if(action.type =='edit_todo'){
+        
+    }
+    else if(action.type =='delete_todo'){
+
+    }
+    else if(action.type =='complete_todo'){
+
+    }
+  return ;
+}
+
+export default TodoReducer
